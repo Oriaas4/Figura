9bd75d5f0f85af271fb8648916418124c69be9f8
sergi <scomacorcuera@iesesteveterradas.cat>
2019-03-20 09:43:14 +0100

    mODIFICACION FIGUFRWA

--- Figura/src/Figuras/Figura.java
+++ Figura/src/Figuras/Figura.java
@@ -3,11 +3,11 @@ package Figuras;
 public abstract class Figura {
 	
 	protected int x;
-	protected int y;
+	protected int z;
 	
 	public Figura(int x,int y) {
 		this.x=x;
-		this.y=y;
+		this.z=y;
 	}
 
 	public abstract double area();	
