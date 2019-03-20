1074c6e666ceeacabc3afeff65a4edb390adb8aa
sergi <scomacorcuera@iesesteveterradas.cat>
2019-03-20 09:33:07 +0100

    Bucle hecho

---
+++ Figura/src/Figuras/Bucle.java
@@ -0,0 +1,13 @@
+package Figuras;
+
+public class Bucle {
+
+	public static void main(String[] args) {
+		int a=1;
+		while(a!=0) {
+			System.out.println("Comeme la nepe "+a+" veces.");
+			a=a+1;
+		}
+	}
+
+}
